7b1201849a19a865a355bcdb02d94836909f7570
Nikhil Mathur <86379900+Mathurshab2210@users.noreply.github.com>
2025-12-15 22:05:54 +0530

    Refactor header.js to update navbar on scroll
    Removed parallax header functionality and added scroll effect for navbar.

--- JS/header.js
+++ JS/header.js
@@ -1,14 +1,9 @@
-window.onload = function () {
-    const header = document.querySelector(".parallax-header");
-    const windowHeight = window.innerHeight;
+const navbar = document.getElementById("navbar");
 
-    // Set header height dynamically
-    header.style.height = windowHeight + "px";
-
-    window.addEventListener("scroll", () => {
-        let scroll = window.scrollY;
-
-        // Move background for parallax effect
-        header.style.backgroundPositionY = (50 - scroll * 0.1) + "%";
-    });
-};
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
